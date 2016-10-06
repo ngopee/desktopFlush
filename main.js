7fa68a41220378ff45e5e8bc8549bd9441b0dcfa
John <jnaguib@cmu.edu>
2016-10-06 21:27:23 +0200

    figuring out the linking between finder and the app

--- main.js
+++ main.js
@@ -2,25 +2,24 @@ const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+
+global.sharedObj = {windows: []};
+
+exports.createWindow = createWindow;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({
+  var win = new BrowserWindow({
       name: "My app window",
     width: 500,
     height: 250,
     transparent: true,
-    toolbar: false
-});
-
-
+    toolbar: false,
+    frame: false
+    });
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -30,12 +29,24 @@ function createWindow () {
 
     win = null
   })
+
+  return win;
+}
+
+
+
+function startApp(){
+    var win = createWindow();
+    // Open the DevTools.
+    win.webContents.openDevTools()
+
+    global.sharedObj.windows.push(win);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', startApp)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -50,6 +61,7 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    startApp();
+
   }
 })
